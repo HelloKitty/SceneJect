36cfb77e9881f83c9d7789ce083f61bac9ad7d95
Andrew Blakely <andrew.blakely@ymail.com>
2016-01-31 20:28:19 -0600

    Test Injector Injection

--- tests/SceneJect.Common.Tests/UnitTests/InjectorTests.cs
+++ tests/SceneJect.Common.Tests/UnitTests/InjectorTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,10 +35,39 @@ namespace SceneJect.Common.Tests
 			Assert.Throws<InvalidOperationException>(() => injector.Inject());
         }
 
+		[Test]
+		public static void Test_Injector_Sets_Values()
+		{
+			//arrange
+			Mock<IResolver> resolver = new Mock<IResolver>(MockBehavior.Strict);
+			TestClass testInstance = new TestClass();
+
+			//set it up to throw on any resolve
+			resolver.Setup(x => x.Resolve(It.IsAny<Type>()))
+				.Returns(new List<int>() { 1, 2, 3 });
+
+			Injector injector = new Injector(testInstance, resolver.Object);
+
+			//assert
+			Assert.DoesNotThrow(() => injector.Inject());
+			Assert.NotNull(testInstance); //make sure it didn't null this somehow lol. Can't without ref
+			Assert.NotNull(testInstance.test);
+
+			for (int i = 1; i < 4; i++)
+			{
+				Assert.IsTrue(testInstance.test.Contains(i));
+				Assert.IsTrue(testInstance.test2.Contains(i));
+			}
+				
+		}
+
 		public class TestClass
 		{
 			[Inject]
-			public IDisposable test;
+			public IList<int> test;
+
+			[Inject]
+			public IList<int> test2;
 		}
 	}
 }
