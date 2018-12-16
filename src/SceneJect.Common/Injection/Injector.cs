c6bda55dd0956e7060380117a3ce4aa4f6c648fa
HelloKitty <andrew.blakely@ymail.com>
2018-12-15 21:03:52 -0600

    Fix netstandard 2.0 Unity2018 missing DynamicMethod issue

--- src/SceneJect.Common/Injection/Injector.cs
+++ src/SceneJect.Common/Injection/Injector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -56,12 +57,31 @@ namespace SceneJect.Common
 					//Based on FreecraftCore's: https://github.com/FreecraftCore/FreecraftCore.Serializer/blob/152fda27c46fcfcaa72d3568c1a591d728773f33/src/FreecraftCore.Serializer.API/Reflection/Serialization/MemberSerializationMediator.cs
 					if (mi is PropertyInfo && !((PropertyInfo)mi).CanWrite)
 					{
+						//TODO: Unity2018 has issues with this FasterFlect PlatformNotSupportedException: Operation is not supported on this platform.
 						//If it's a property and it's a readonly one we'll try to grab the backing field
-						ObjectInstance.SetFieldValue($"<{mi.Name}>k__BackingField", Resolver.Resolve(mi.Type()));
+						//ObjectInstance.SetFieldValue($"<{mi.Name}>k__BackingField", Resolver.Resolve(mi.Type()));
+						FieldInfo field = ObjectType.GetField($"<{mi.Name}>k__BackingField", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField);
+						if(field == null)
+							throw new InvalidOperationException($"Unable to get backing field for {mi.Name}");
+
+						field.SetValue(ObjectInstance, Resolver.Resolve(mi.Type()), BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetField, null, CultureInfo.CurrentCulture);
+						//ObjectInstance.SetFieldValue($"<{mi.Name}>k__BackingField", Resolver.Resolve(mi.Type()));
 					}
 					else
-						mi.Set(ObjectInstance, Resolver.Resolve(mi.Type()));
+					{
+						//TODO: Unity2018 has issues with this FasterFlect PlatformNotSupportedException: Operation is not supported on this platform.
+						//mi.Set(ObjectInstance, Resolver.Resolve(mi.Type()));
+						if(mi is PropertyInfo p)
+							p.SetValue(ObjectInstance, Resolver.Resolve(mi.Type()));
+						else if(mi is FieldInfo f)
+							f.SetValue(ObjectInstance, Resolver.Resolve(mi.Type()));
+					}
 				}
+
+				//If we didn't fail we should invoke the dependency injection callbacks
+				//if the object implemented them
+				if(this.ObjectInstance is IDependencyInjectionCallbackable dpc)
+					dpc.OnDependencyInjectionFinished();
 			}
 			catch (Exception e)
 			{
