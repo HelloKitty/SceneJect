ea1b5140e820abd2fbcfea37a60d389395c8a8f1
HelloKitty <andrew.blakely@ymail.com>
2018-12-15 21:05:11 -0600

    Added dependency injection callback interface

---
+++ src/SceneJect.Metadata/Interfaces/IDependencyInjectionCallbackable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SceneJect.Common
+{
+	/// <summary>
+	/// Contract for types that are listening
+	/// for dependency injection service callbacks.
+	/// </summary>
+	public interface IDependencyInjectionCallbackable
+	{
+		/// <summary>
+		/// Callback invoked when the object has
+		/// had its dependencies marked with <see cref="InjectAttibute"/> injected.
+		/// </summary>
+		void OnDependencyInjectionFinished();
+	}
+}
